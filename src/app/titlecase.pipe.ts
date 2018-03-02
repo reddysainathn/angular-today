504a51a068a9566e15a0441cfd3e6f0334b46953
Sainath <10643404+reddysainathn@users.noreply.github.com>
2018-03-01 22:32:04 -0600

    Assignment 3 Working--> Making Some CHanges and Formatting

--- src/app/titlecase.pipe.ts
+++ src/app/titlecase.pipe.ts
@@ -6,20 +6,26 @@ export class TitleCasePipe implements PipeTransform {
     transform(value: string, args?: any) {
         if (!value)
             return null;
-        //let actualLimit = (limit) ? limit : 50;
-        //value = value.toUpperCase();
-        let prepositons = [
-            'of', 'the'
-        ];
         let arr = value.split(" ");
         for (let i: number = 0; i < arr.length; i++) {
-            if (i > 0 && prepositons.includes(arr[i].toLowerCase())) {
+            if (i > 0 && this.isPreposition(arr[i])) {
                 arr[i] = arr[i].toLowerCase();
             } else {
-                arr[i] = arr[i].substr(0, 1).toUpperCase() + arr[i].substring(1, arr[i].length).toLowerCase();
+                arr[i] = this.toTitleCase(arr[i]);
             }
         }
         console.log(arr)
         return arr.join(' ');
+    }
+
+    private isPreposition(word: String): boolean {
+        let prepositons = [
+            'of', 'the'
+        ];
+        return prepositons.includes(word.toLowerCase());
+    }
+
+    private toTitleCase(text:string):string{
+        return text.substr(0, 1).toUpperCase() + text.substring(1, text.length).toLowerCase();
     }
 }
